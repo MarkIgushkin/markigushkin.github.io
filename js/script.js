84a9e2a0d5b7720845051f4bb950d8c0c017cc77
Mark <igushkin.m@gmail.com>
2022-12-14 16:07:38 +0000

    Final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     // Form custom validation
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    // Documentation here - https://getbootstrap.com/docs/5.0/forms/validation/
     var forms = document.querySelectorAll('.needs-validation')
     
     // Loop over them and prevent submission
@@ -8,8 +9,17 @@ $(document).ready(function(){
         .forEach(function (form) {
             form.addEventListener('submit', function (event) {
                 if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
+                    event.preventDefault();
+                    event.stopPropagation();
+                }else{
+                    event.preventDefault();
+                    event.stopPropagation();
+                    
+                    if($(form).attr('id') == 'subscription-form'){
+                        new bootstrap.Modal($("#sub-modal")).toggle();
+                    }else{
+                        new bootstrap.Modal($("#form-modal")).toggle();
+                    }
                 }
                 form.classList.add('was-validated')
             }, false)
@@ -17,16 +27,18 @@ $(document).ready(function(){
 
 
 
+    //Filtering products in the catalog
     products = $('.catalog .product');
-
 
     $('input[type=radio][name=filter]').change(function() {
         
         duration = 500;
 
+        //Show all products
         if(this.value == 'all'){
             products.show(duration);
         }
+        //Show option1
         else if (this.value == 'option1') {
             products.each(function(){
                 if($(this).hasClass('option1')){
@@ -36,6 +48,7 @@ $(document).ready(function(){
                 }
             });
         }
+        //Show option2
         else if(this.value == 'option2'){
             products.each(function(){
                 if($(this).hasClass('option2')){
@@ -45,6 +58,7 @@ $(document).ready(function(){
                 }
             });
         }
+        //Show option3
         else if(this.value == 'option3'){
             products.each(function(){
                 if($(this).hasClass('option3')){
@@ -57,12 +71,10 @@ $(document).ready(function(){
     });
 
 
-    //const map = new Map();
-
-    //map.set('a', 1);
-
+    //Adding products to local storage
     $("#add-to-card").click(function(event){
 
+        //Parsing JSON from local storage
         values = JSON.parse(window.sessionStorage.getItem('map'));
 
         if(values == null){
@@ -71,11 +83,13 @@ $(document).ready(function(){
             map = new Map(Object.entries(values));
         }
 
+        // Get data about product
         productId = $("#product-info").attr("data-product-id");
         productPrice = $("#product-info").attr("data-price");
         prodcutName = $("h1").text();
-        src = "../images/product" + productId + "-min.jpg";
+        src = "../images/product" + productId + "-min.webp";
 
+        // Save new product t the map
         if(map.has(productId)){
             obj = map.get(productId);
             obj.quantity = obj.quantity + 1;
@@ -93,13 +107,16 @@ $(document).ready(function(){
             console.log(obj);
         }
 
+        //Saving JSON to local storage with new product
         window.sessionStorage.setItem('map', JSON.stringify(Object.fromEntries(map)));
 
+        // Udate card for the user
         updateCard();
 
         new bootstrap.Modal($("#added")).toggle();
     }); 
 
+    // Order class that stores info about order
     class Order {
         id;
         quantity;
@@ -108,19 +125,23 @@ $(document).ready(function(){
         name;
     }
 
+    //On click remove products from card
     $("#remove-app").click(function(){
         clearStorage();
     });
 
 
+    //Update card and update checkout page every time the page loads
     updateCard();
     updateCheckoutPage();
 
+    //Clear storage function
     function clearStorage(){
         window.sessionStorage.setItem('map', '{}');
         location.reload();
     };
 
+    //Update card function
     function updateCard() {
         counter = 0;
 
@@ -139,10 +160,12 @@ $(document).ready(function(){
         $('#quantity').text(counter);
     }
 
+    //Update checkout page 
     function updateCheckoutPage() {
         counter = 0;
         total = 0;
 
+        // No items have been added
         if(jQuery.isEmptyObject(values)){
             $("#summary").append(
                 "<div class=\"col-12 no-items\">No items have been added yet!</div>"
@@ -150,7 +173,9 @@ $(document).ready(function(){
             $("#remove-itm-btn").hide();
             $("#checkout-btn").prop("disabled", true);
             return;
-        }else{
+        }
+        //Add all added items to page
+        else{
             map = new Map(Object.entries(values));
 
             map.forEach(value => {
@@ -170,5 +195,17 @@ $(document).ready(function(){
                 new bootstrap.Modal($("#removed")).toggle();
             });
         }
-    }
+    };
+
+    //Reload page after cloasing subscription modal
+    $('#sub-modal').on('hidden.bs.modal', function (e) {
+        location.reload();
+    });
+    //Reload page after cloasing order modal
+    $('#form-modal').on('hidden.bs.modal', function (e) {
+        if($('#form-modal').data('key') === 'checkout'){
+            window.sessionStorage.setItem('map', '{}');
+        }
+        location.reload();
+    });
 });
